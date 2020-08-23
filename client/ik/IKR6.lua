1a349f7137240341b374468704bf39a26f7eccff
lego man <rajawilliams24@gmail.com>
2020-08-22 19:13:15 -0700

    Update IKR6.lua

--- client/ik/IKR6.lua
+++ client/ik/IKR6.lua
@@ -4,4 +4,18 @@ local InverseKinematics = require(script:WaitForChild("InverseKinematics"))
 
 local IK6 = {}
 
+function IK6.AutoRig(Rig)
+	-- ah yes. spaghet.
+	local Humanoid = assert(Rig:FindFirstChild("Humanoid"), "Humanoid not found!")
+	assert(Humanoid.RigType == Enum.HumanoidRigType.R6, "Not a valid R6 rig!")
+	assert(Rig:FindFirstChild("HumanoidRootPart"), "HumanoidRootPart not found!")
+	assert(Rig:FindFirstChild("Torso"), "Torso not found!")
+	assert(Rig:FindFirstChild("Left Leg"), "Left Leg not found!")
+	assert(Rig:FindFirstChild("Left Arm"), "Left Arm not found!")
+	assert(Rig:FindFirstChild("Right Leg"), "Right Leg not found!")
+	assert(Rig:FindFirstChild("Right Arm"), "Right Arm not found!")
+
+	
+end
+
 return IK6
