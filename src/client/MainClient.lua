09ccf73dd2203b06e9c494c18ae1d78662fd72f7
lego man <khepri@kephri.local>
2020-08-30 20:40:00 -0700

    Endlessly test CI

--- src/client/MainClient.lua
+++ src/client/MainClient.lua
@@ -4,10 +4,14 @@
 	TODO: Seperate the standalone and plugin codebases. Should be fairly simple
 	as most of the code is highly modular.
 ]]
-local KeySequence = require(script:WaitForChild("KeySequence"))
-local Animation = require(script:WaitForChild("Animation"))
-local MainUI = require(script:WaitForChild("MainUI"))
+local client = script:FindFirstAncestor("client")
+local anim = client:WaitForChild("anim")
+local ui = client:WaitForChild("ui")
 
-local MainAnimation = Animation.new() -- TODO: saving
+local KeySequence = require(anim:WaitForChild("KeySequence"))
+local Animation = require(anim:WaitForChild("Animation"))
+local MainUI = require(ui:WaitForChild("MainUI"))
+
+local MainAnimation = Animation.new()
 
 MainUI.Initialize()
